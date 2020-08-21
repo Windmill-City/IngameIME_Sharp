9ed1850179abd4eda904c5978ee5dcee7bd9cbb6
Windmill-City <1449182174@qq.com>
2020-08-21 23:53:46 +0800

    Only handle compstr when UILess Mode

--- ImeSharp/IMM32_IMEControl.cs
+++ ImeSharp/IMM32_IMEControl.cs
@@ -296,7 +296,7 @@ namespace ImeSharp
         /// <param name="genrealFlag">Modify Flag for Composition Text or Result Text</param>
         private void Handle_COMPOSITION_Msg(long compFlag, long genrealFlag)
         {
-            if ((GCS_COMPSTR & compFlag) != 0 && !_isUIElementOnly)//Comp String/Sel Changed
+            if ((GCS_COMPSTR & compFlag) != 0 && _isUIElementOnly)//Comp String/Sel Changed
             {
                 //CompStr
                 int size = ImmGetCompositionString(_immContext, GCS_COMPSTR, null, 0);
