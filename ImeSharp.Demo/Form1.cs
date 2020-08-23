3897807464dd2cd456388ab9050bd8449be3267f
Windmill-City <1449182174@qq.com>
2020-08-24 01:55:44 +0800

    Add comment

--- ImeSharp.Demo/Form1.cs
+++ ImeSharp.Demo/Form1.cs
@@ -22,7 +22,7 @@ namespace ImeSharp.Demo
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            iMEControl = ImeSharp.Get_IMM32Control();
+            iMEControl = ImeSharp.Get_IMM32Control();//test IMM32 first, or TF API will disable IMM32 API
             initIMEControl(uiless);
         }
 
