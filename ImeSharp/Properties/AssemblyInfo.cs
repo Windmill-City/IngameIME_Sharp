4da71b451e1dd3aa954f3ee54b0a7cd754c344cf
Windmill-City <1449182174@qq.com>
2020-08-28 15:40:45 +0800

    update proj file refs

--- ImeSharp/Properties/AssemblyInfo.cs
+++ ImeSharp/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ImeSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("For CSharp application using IME")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ImeSharp")]
